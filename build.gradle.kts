b6cd0331af06863f84fa08dd026706c4e379fef0
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2026-09-17 20:38:50 -0500

    Upgrade build and CI dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmDefaultMode
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.4.20"
-    id("org.jetbrains.intellij.platform") version "2.18.1"
+    id("org.jetbrains.intellij.platform") version "2.19.0"
 }
 
 group = providers.gradleProperty("pluginGroup").get()
